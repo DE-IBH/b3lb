64152d9e3708eb2d7d25369053fb7e91f7dbeda2
Kilian, Philipp <kilian@ibh.de>
2021-04-11 00:53:12 +0200

    Escape non-existing asset case.

--- b3lb/rest/b3lb/endpoints.py
+++ b3lb/rest/b3lb/endpoints.py
@@ -18,7 +18,7 @@
 import aiohttp
 from aiohttp.web_request import URL
 from asgiref.sync import sync_to_async
-from rest.models import Meeting, Metric, Stats, SecretMeetingList
+from rest.models import Meeting, Metric, Stats, SecretMeetingList, Asset
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.core.exceptions import ObjectDoesNotExist
 import rest.b3lb.lb as lb
@@ -151,15 +151,21 @@ async def create(request, endpoint, params, node, secret):
 
     # check for custom logo
     if "logo" not in params:
-        if secret.tenant.asset and secret.tenant.asset.logo:
-            params["logo"] = secret.tenant.asset.logo_url
+        try:
+            if secret.tenant.asset and secret.tenant.asset.logo:
+                params["logo"] = secret.tenant.asset.logo_url
+        except Asset.DoesNotExist:
+            pass
 
     params = await sync_to_async(lb.check_parameter)(params, secret.tenant)
 
     if request.method == "GET":
-        if secret.tenant.asset and secret.tenant.asset.slide:
-            body = '<modules><module name="presentation"><document url="{}" filename="{}"></document></module></modules>'.format(secret.tenant.asset.slide_url, secret.tenant.asset.s_filename)
-            request.method = "POST"
+        try:
+            if secret.tenant.asset and secret.tenant.asset.slide:
+                body = '<modules><module name="presentation"><document url="{}" filename="{}"></document></module></modules>'.format(secret.tenant.asset.slide_url, secret.tenant.asset.s_filename)
+                request.method = "POST"
+        except Asset.DoesNotExist:
+            pass
 
     response = await pass_through(request, endpoint, params, node, body=body)
 
