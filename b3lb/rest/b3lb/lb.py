11afce27164cd818a92795b04e81910bc5410901
Thomas Liske <thomas@fiasko-nw.net>
2021-04-10 23:18:21 +0200

    Fix typo in b3lb settings names.

--- b3lb/rest/b3lb/lb.py
+++ b3lb/rest/b3lb/lb.py
@@ -30,7 +30,7 @@ from django.conf import settings
 ##
 # CONSTANTS
 ##
-slug_regex = re.compile(r'([a-z]{2,10})(-(\d{3}))?\.' + re.escape(settings.B3LP_API_BASE_DOMAIN) + '$')
+slug_regex = re.compile(r'([a-z]{2,10})(-(\d{3}))?\.' + re.escape(settings.B3LB_API_BASE_DOMAIN) + '$')
 
 # symbols not to be encoded to match bbb's checksum calculation
 SAFE_QUOTE_SYMBOLS = '*'
