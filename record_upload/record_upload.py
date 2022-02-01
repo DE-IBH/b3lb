60e629d73a517607d05184a15680c227c2d1796e
Kilian, Philipp <kilian@ibh.de>
2022-02-01 15:16:27 +0100

    Show file output.

--- record_upload/record_upload.py
+++ record_upload/record_upload.py
@@ -17,32 +17,57 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
-import sys
 import os
-import subprocess as sp
-import tarfile
+import io
 import requests
+import subprocess as sp
+import sys
+import tarfile
+
 
 if len(sys.argv) < 2:
     exit()
 
 B3LB_BASE_DOMAIN = "https://api-test.bbbconf.de/"
 PUBLISH_FOLDER = "/var/bigbluebutton/published/presentation"
-
 published_presentations = os.listdir(PUBLISH_FOLDER)
 
-meeting_id = sys.argv[1]
+internal_meeting_id = sys.argv[1]
+if internal_meeting_id not in published_presentations:
+    exit()
+
+meeting_id = ""
 
 bbb_record_listing = sp.check_output(["bbb-record", "--list"]).decode("utf-8").split("\n")[2:]
+for line in bbb_record_listing:
+    if line[:2] == "--":
+        break
+    if line.split(" ")[0] == internal_meeting_id:
+        meeting_id = line.split(" ")[-1]
 
-tar_filename = "{}/{}.tar.xz".format(PUBLISH_FOLDER, meeting_id)
+tar_filename = "{}/{}.tar.xz".format(PUBLISH_FOLDER, internal_meeting_id)
+tar_file = io.BytesIO()
 
 with open("/var/log/bigbluebutton/post_publish.log", "a") as f:
-    f.write("B3LB Start Upload routine for {} received\n".format(meeting_id))
-    tar = tarfile.open(tar_filename, mode="w|xz")
-    tar.add("{}/{}".format(PUBLISH_FOLDER, meeting_id))
+    f.write("B3LB Start Upload routine for {} received\n".format(internal_meeting_id))
+    tar = tarfile.open(fileobj=tar_file, mode="w|xz")
+    # tar = tarfile.open(tar_filename, mode="w|xz")
+    tar.add("{}/{}".format(PUBLISH_FOLDER, internal_meeting_id))
     tar.close()
-    f.write("B3LB Upload files compressed to {}\n".format(tar_filename))
+    f.write("B3LB Upload files compressed.\n".format(tar_filename))
 
-    response = requests.post("{}b3lb/b/record/upload", files={"file": open(tar_filename, "rb")})
-    f.write("B3LB Upload Response Code: {}".format(response.status_code))
+    if meeting_id:
+        # response = requests.post("{}b3lb/b/record/upload", files={"file": open(tar_filename, "rb")}, data={"meeting_id": meeting_id})
+        response = requests.post("{}b3lb/b/record/upload", files={"file": tar_file.getvalue()}, data={"meeting_id": meeting_id})
+    else:
+        # response = requests.post("{}b3lb/b/record/upload", files={"file": open(tar_filename, "rb")})
+        response = requests.post("{}b3lb/b/record/upload", files={"file": tar_file.getvalue()})
+
+    if response.status_code == 204:
+        f.write("B3LB Upload successful. Deleting record {}\n".format(internal_meeting_id))
+        sp.check_output(["bbb-record", "--delete", internal_meeting_id])
+        # os.remove("{}/{}".format(PUBLISH_FOLDER, tar_filename))
+    else:
+        f.write("B3LB Upload failed. Keeping record files {}\n".format(internal_meeting_id))
+        # os.remove("{}/{}".format(PUBLISH_FOLDER, tar_filename))
+
