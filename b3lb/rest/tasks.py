07d9fc98a5d4396c994a1ded1ddf7d43958907b7
Kilian, Philipp <kilian@ibh.de>
2022-03-29 16:27:01 +0200

    Move queue definition to app.task decorator.

--- b3lb/rest/tasks.py
+++ b3lb/rest/tasks.py
@@ -27,41 +27,41 @@ from rest.models import Node, Tenant, Secret, RecordSet
 logger = get_task_logger(__name__)
 
 
-@app.task(ignore_result=True, base=Singleton)
+@app.task(ignore_result=True, base=Singleton, queue='node')
 def check_node(node_uuid):
     return celery_run_check_node(node_uuid)
 
 
-@app.task(ignore_result=True, base=Singleton, name="Check Node Status")
+@app.task(ignore_result=True, base=Singleton, name="Check Node Status", queue='node')
 def check_status():
     for node in Node.objects.all():
         check_node.si(str(node.uuid)).apply_async()
     return True
 
 
-@app.task(ignore_result=True, base=Singleton, name="Cleanup Assets")
+@app.task(ignore_result=True, base=Singleton, name="Cleanup Assets", queue='cleanup')
 def cleanup_assets():
     return celery_cleanup_assets()
 
 
-@app.task(ignore_result=True, base=Singleton, name="Render not rendered Record Sets")
+@app.task(ignore_result=True, base=Singleton, name="Render not rendered Record Sets", queue='recording')
 def render_record_sets():
     for record_set in RecordSet.objects.filter(status=RecordSet.UPLOADED):
         celery_render_records(record_set)
     return True
 
 
-@app.task(ignore_result=True, base=Singleton)
+@app.task(ignore_result=True, base=Singleton, queue='secret')
 def update_secret_meetings_lists(secret_uuid):
     return celery_update_get_meetings_xml(secret_uuid)
 
 
-@app.task(ignore_result=True, base=Singleton)
+@app.task(ignore_result=True, base=Singleton, queue='secret')
 def update_secret_metrics_list(secret_uuid):
     return celery_update_metrics(secret_uuid)
 
 
-@app.task(ignore_result=True, base=Singleton, name="Update Secrets List")
+@app.task(ignore_result=True, base=Singleton, name="Update Secrets List", queue='secret')
 def update_secrets_lists():
     update_secret_metrics_list.si(None).apply_async()
     for secret in Secret.objects.all():
@@ -70,13 +70,13 @@ def update_secrets_lists():
     return True
 
 
-@app.task(ignore_result=True, base=Singleton, name="Update Statistics")
+@app.task(ignore_result=True, base=Singleton, name="Update Statistics", queue='statistic')
 def update_statistic():
     for tenant in Tenant.objects.all():
         update_tenant_statistic.si(str(tenant.uuid)).apply_async()
     return True
 
 
-@app.task(ignore_result=True, base=Singleton, name="Update Tenant Statistics")
+@app.task(ignore_result=True, base=Singleton, name="Update Tenant Statistics", queue='statistic')
 def update_tenant_statistic(tenant_uuid):
     return celery_statistic_fill_by_tenant(tenant_uuid)
