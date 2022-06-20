16a7f399abfa44aed97f900393429834c189de51
Philipp Kilian <kilian@ibh.de>
2022-06-20 13:15:16 +0200

    Change deprecated import of "url" with "re_path".

--- b3lb/loadbalancer/urls.py
+++ b3lb/loadbalancer/urls.py
@@ -16,22 +16,21 @@
 
 
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path, include, re_path
 from rest.views import api_pass_through, stats, ping, metrics, slide, logo, custom_css
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^bigbluebutton/api/(?P<endpoint>[0-9.a-zA-Z]*)$', api_pass_through),
+    re_path(r'^bigbluebutton/api/(?P<endpoint>[0-9.a-zA-Z]*)$', api_pass_through),
     path('b3lb/stats', stats),
     path('b3lb/metrics', metrics),
     path('b3lb/ping', ping),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/bbb/api/(?P<endpoint>[0-9.a-zA-Z]*)$', api_pass_through),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/stats', stats),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/metrics', metrics),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})/logo', logo),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})/slide', slide),
-    url(r'^b3lb/t/(?P<slug>[a-z]{2,10})/css', custom_css),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/bbb/api/(?P<endpoint>[0-9.a-zA-Z]*)$', api_pass_through),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/stats', stats),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})(-(?P<sub_id>\d{3}))?/metrics', metrics),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})/logo', logo),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})/slide', slide),
+    re_path(r'^b3lb/t/(?P<slug>[a-z]{2,10})/css', custom_css),
     # necessary for db-file-storage extension!
-    url(r'^files/', include('db_file_storage.urls'))
+    re_path(r'^files/', include('db_file_storage.urls'))
 ]
