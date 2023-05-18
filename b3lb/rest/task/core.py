79c950143abc5746ba55ac6960075fa0c221f0ff
Philipp Kilian <kilian@ibh.de>
2023-05-19 00:19:27 +0200

    async: fix cluster group filtering check in node status check

--- b3lb/rest/task/core.py
+++ b3lb/rest/task/core.py
@@ -86,6 +86,8 @@ def check_node(check: NodeCheck):
                                             check.meeting_stats[meeting_id][cell.tag] = cell.text
                             check.attendees += attendee_dummy
             check.has_errors = False
+        else:
+            print(response.text)
     except:
         pass
 
